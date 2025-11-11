023d8efb07c2286e8d1507626d124c1d5102cdb7
PrabeeSohal <prabeersohal@gmail.com>
2025-11-11 16:39:57 -0500

    Done

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,20 @@
 package lab1;
 
 public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples(1000,3,5));
+
+    }
+
+    public static int multiples(int n, int a, int b) {
+        if(a == b){
+            return (n-1)/a;
+        }
+        else {
+            int count3 = (n-1)/a;
+            int count5 = (n - 1) / b;
+            int count15 = (n - 1) / (a * b);
+            return count3 + count5 - count15;
+        }
+    }
 }
