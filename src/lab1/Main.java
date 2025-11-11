5387e23f7ce0684ed989de5cb29d31720ed3299e
PrabeeSohal <prabeersohal@gmail.com>
2025-11-10 23:58:59 -0500

    Done task 3

---
+++ src/lab1/Main.java
@@ -0,0 +1,8 @@
+
+package lab1;
+
+public class Main {              // <- class definition starts here
+    public static void main(String[] args) {
+        FizzBuzz.main(args); // <- code lives inside the class
+    }
+}                                 // <- class definition ends here
